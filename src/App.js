d5ed6cd6f9b59ca21a6fc6c0341394c8094f4c2f
prerak <prerak@gmail.com>
2023-12-15 21:21:54 +0530

    APPS.JS NOW CONTAINS COMMENTS FOR EACH DEV

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import './App.css';
-import { BrowserRouter ,Routes, Route} from 'react-router-dom';
-import Signup from './pages/signup.js';
-import Home from './pages/home.js'
+// ...........PARVEEN NOTES...............
+// -->
+
+//..............................................................
+// .............................................................
+
+// ...........PRERAK NOTES...............
+// -->
+
+//..............................................................
+// .............................................................
+
+// ...........AAYUSH NOTES...............
+
+//-->
+
+//..............................................................
+// .............................................................
+
+// ...........AARAV NOTES...............
+// -->
+
+//..............................................................
+// .............................................................
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/signup.js";
+import Home from "./pages/home.js";
 export default function App() {
   return (
-    <><BrowserRouter>
-    <Routes>
-      
-    <Route path="/signup" element={<Signup/>}/>
-    <Route index element={<Home/>}/>
-    
-    
-    </Routes>
-    </BrowserRouter> 
-</>
-    
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
-
