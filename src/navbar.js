716d1e17b518b72822263009c46634a555aa5d7f
Parveen Rawat <rawatparveen663@gmail.com>
2023-12-16 16:45:41 +0530

    changed signup page and moved signup page css to pages/signup.css and minor tweaks in navbar.js

--- src/navbar.js
+++ src/navbar.js
@@ -1,19 +1,19 @@
 import './App.css'
 import { useNavigate } from 'react-router-dom';
-import Signup from './pages/signup';
+// import Signup from './pages/signup';
 export default function Navbar(){
     const navigate=useNavigate();
     return (
 
         <><div class="navbar">
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li ><button className='navbutton' onClick={()=>navigate("/")}>Home</button></li>
+          {/* change where the navigation buttons will take u here 👇 */}
+          <li ><button className='navbutton' onClick={()=>navigate("/")}>About</button></li>
+          <li ><button className='navbutton' onClick={()=>navigate("/")}>Contact</button></li>
         </ul>
         <div>
-          <button onClick={()=>navigate("/signup")}>Login</button>&nbsp;
-          
+          <button className = "Login" onClick={()=>navigate("/signup")}>Login</button>&nbsp;
         </div>
       </div></>
     )
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,27 +1,54 @@
+// importing all the required elements 
 import Navbar from "../navbar.js"
+import "./signup.css"
+import { useState } from "react"
+
+//imports for font awesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//for more icons add name here 👇
+import { faEnvelope,faUser,faLock } from '@fortawesome/free-solid-svg-icons' 
+
+
 export default function Signup(){
-    return (
-<><Navbar/>
-<div className="container">
-<div className="signup-container">
-    <h2>Signup</h2>
-    <form>
-      <div className="form-group">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required/>
-      </div>
-      <div className="form-group">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required/>
-      </div>
-      <div className="form-group">
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required/>
-      </div>
-      <button type="submit">Signup</button>
-      <button type="button" class="login-btn">Login</button>
-    </form>
-  </div></div>
-</>
+	const [action,setAction] = useState("Login");
+	return (
+		<div>
+		<Navbar/>
+		<div className='container'>
+        <div className='header'>
+            <div className='text'>{action}</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+			<div className="input">
+				<div className="img">
+				<FontAwesomeIcon icon={faUser} />
+				</div>
+				<input type="text" placeholder='Enter your username'/>
+			</div>
+			{action==="Login"?<div></div>:
+			<div className="input">
+				<div className="img">
+					<FontAwesomeIcon icon={faEnvelope} />
+				</div>
+				<input type="email" placeholder='Enter your email'/>
+			</div>
+			}
+			<div className="input">
+				<div className="img">
+					<FontAwesomeIcon icon={faLock} />
+				</div>
+				<input type="password" placeholder='Enter your password'/>
+			</div>
+		</div>
+		{action==="Sign Up"?<div></div>:
+			<div className="forgot-password">forgot password ?<span> click here </span></div>
+		}
+		<div className="submit-container">
+			<div className={action==="Login"?"submit gray":"submit"} onClick={()=>setAction("Sign Up")}>Signup</div>
+			<div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>setAction("Login")}>Login</div>
+		</div>
+    </div>
+  	</div>
     )
 }
