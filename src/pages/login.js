eeaf756056a5b18e915129ebeac113c3551f4ed4
prerak <prerak@gmail.com>
2023-12-21 03:03:49 +0530

    added login using google(with official button)

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import "./login.css";
-import { auth ,provider} from "../firebase";
+import { auth, provider } from "../firebase";
 import { NavLink, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword ,signInWithRedirect} from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
+import { GoogleButton } from "react-google-button";
 //function starts here
-const Login = () => {
+export default function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  // const [value,setValue] = useState('')
-  const handleClick =()=>{
-      signInWithRedirect(auth,provider);
-  }
+  const handleClick = (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
 
-  
   const onLogin = (e) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
@@ -31,46 +38,55 @@ const Login = () => {
         console.log(password);
       });
   };
-  
+
   return (
     <div className="container">
-    <div className="loginform">
-      <form>
-        <input
-          type="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-          placeholder="Enter Email"
-        />
-        <br />
-
-        <input
-          placeholder="Enter Username"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-        />
-        <br />
-
-        <input
-          placeholder="Enter Password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-        <br />
-
-        <br />
-
-        <button type="submit" onClick={onLogin}>
-          LOGIN
-        </button>
-        <button onClick={handleClick}>
-          sign in with google
-        </button>
-        <NavLink to="/signup">Signup</NavLink>
-      </form>
-      
-    </div></div>
+      <div className="loginform">
+        <form>
+          <input
+            type="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="Enter Email"
+          />
+          <br />
+          <input
+            placeholder="Enter Username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+          <br />
+          <input
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <br />
+          <button type="submit" onClick={onLogin}>
+            Login
+          </button>{" "}
+          <GoogleButton onClick={handleClick} />
+        </form>
+      </div>
+    </div>
   );
-};
-export default Login;
+}
+
+// import React, { useEffect, useState } from "react";
+// import { auth, provider } from "../firebase";
+// import { signInWithPopup } from "firebase/auth";
+
+// function SignIn() {
+//   const [value, setValue] = useState("");
+//   const handleClick = () => {
+//     signInWithPopup(auth, provider);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleClick}>Signin With Google</button>
+//     </div>
+//   );
+// }
+// export default SignIn;
